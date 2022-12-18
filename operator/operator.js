2e7be02561a103b7468f1d1da8a74428b46c9e60
Admin <kavyak6999@gmail.com>
2022-12-18 11:34:17 +0530

    logical operator

---
+++ operator/operator.js
@@ -0,0 +1,22 @@
+/*var a = parseInt(prompt("Enter the value of a"));
+var b = parseInt(prompt("Enter the value of b"));
+var d = parseInt(prompt("Enter the value of d"));*/
+
+var a = prompt("Enter the value of a");
+var b = prompt("Enter the value of b");
+
+var c = a&&b;
+//var c = (a>b) && (b<d)
+//var c = a||b;
+//var c = !a;
+alert (c);
+
+/*var a="true";
+if(!a)
+{
+	alert("k");
+}
+else
+{
+	alert("s");
+}*/
